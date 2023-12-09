1326971986374decd3c376bca1c376c120d5a4a2
EdisonGarcía5 <edisongarcia542003@gmail.com>
2023-12-09 11:03:03 -0500

    Actividad

--- Actividad 1 Segundo Parcial/ecommerce.sql
+++ Actividad 1 Segundo Parcial/ecommerce.sql
@@ -12,20 +12,13 @@ CREATE TABLE administrador(
     apm_administrador VARCHAR(50),
     correo_electronico VARCHAR(100),
     contrasena VARCHAR(50),
-    -- nuevos atributos agregados 
-    numero_telefono VARCHAR(20), --para almacenar el número de teléfono del administrador.
-    direccion VARCHAR(100) -- Incluir un campo para almacenar la dirección del administrador.
-    --
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Tabla estatus
 CREATE TABLE estatus(
     id_estatus INT PRIMARY KEY AUTO_INCREMENT,
-    nombre_estatus VARCHAR(25),
-    -- nuevos atributos agregados 
-    fecha_creacion DATE, --para registrar la fecha en que se creó el estatus.
-    estado VARCHAR(10) --para indicar si el estatus está activo o inactivo.
+    nombre_estatus VARCHAR(25)
 );
 
 -- Tabla vendedor
@@ -35,7 +28,7 @@ CREATE TABLE vendedor(
     app_vendedor VARCHAR(50),
     apm_vendedor VARCHAR(50),
     correo_electronico VARCHAR(100),
-    contraseña VARCHAR(50),
+    contrasena VARCHAR(50),
     telefono VARCHAR(10),
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     id_estatus INT,
@@ -46,9 +39,6 @@ CREATE TABLE vendedor(
 CREATE TABLE marca(
     id_marca INT PRIMARY KEY AUTO_INCREMENT,
     nombre_marca VARCHAR(50),
-    -- nuevos atributos agregados 
-    categoria VARCHAR(50) -- es la categoría a la que pertenece la marca ya sea electrónicos, producto de limpiesa, etc.
-    --
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -108,10 +98,6 @@ CREATE TABLE venta(
     id_producto INT,
     cantidad_producto INT,
     fecha_venta TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    -- nuevos atributos agregados 
-    metodo_pago VARCHAR(50), -- el método de pago utilizado en la venta.
-    estado_venta VARCHAR(50), -- el estado actual de la venta, por ejemplo, pendiente, pagada, entregada, etc.
-    --
     FOREIGN KEY(id_vendedor) REFERENCES vendedor(id_vendedor),
     FOREIGN KEY(id_cliente) REFERENCES cliente(id_cliente),
     FOREIGN KEY(id_producto) REFERENCES producto(id_producto)
